292943e34c0df364195a83b4376fb5a6f60f2ae4
CrashingCloud <18957760988@163.com>
2025-10-20 22:07:28 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,15 @@
-//
-//
-//
+//202510304106
+//18957760988@163.com
+//叶子恒
 #include <stdio.h>
-int main()
+
+int main(void)
 {
-    printf("Hello World");
+    printf("C：");
+    float c,f;
+    scanf("%f",&c);
+    f=c*9/5+32; 
+    printf("F：%.1f",f);
+    
     return 0;
 }
