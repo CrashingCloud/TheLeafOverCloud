2ef2cc294e7580d35389438dd5a9a7644ac72285
CrashingCloud <18957760988@163.com>
2025-10-30 21:21:12 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,15 +1,32 @@
 //202510304106
 //18957760988@163.com
 //叶子恒
-#include <stdio.h>
-
+include<stdio.h> 
 int main(void)
 {
-    printf("C：");
-    float c,f;
-    scanf("%f",&c);
-    f=c*9/5+32; 
-    printf("F：%.1f",f);
-    
+    int n = 0;
+    int k = 0;
+    printf("n=");
+    scanf("%d",&n);
+    if (n % 2 == 0)
+    {
+        printf("密钥不安全，请重新输入");
+        return 0;
+    }
+    for (int i = 1;i<50;i+=2)
+    {
+       if (n%i==0)
+       {
+            k+=1;
+       }
+    }
+    if (k<=2)
+    {
+        printf("密钥安全，密码设置成功");
+    }
+    else
+    {
+        printf("密钥不安全，请重新输入");
+    }
     return 0;
-}
+}                                                                                         
