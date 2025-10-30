183e6a2843fdf58638990148dd0324af88f37241
CrashingCloud <18957760988@163.com>
2025-10-30 21:25:10 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -1,36 +1,36 @@
 //202510304106
 //18957760988@163.com
 //叶子恒
-#include <stdio.h>
-
+#include<stdio.h>
 int main(void)
 {
-    int a,b,d;
-    char c;
-    scanf("%d %d %c",&a,&b,&c);
-    switch(c)
+    int n = 0;
+    int i = 1;
+    int cnt = 0;
+    printf("n=");
+    scanf("%d",&n);
+    if (n % 2 == 0)
     {
-        case '+':
-            d=a+b;
-            printf("%d",d);
-            break;
-        case '-':
-            d=a-b;
-            printf("%d",d);
-            break;
-        case '*':
-            d=a*b;
-            printf("%d",d);
-            break;
-        case '/':
-            d=a/b;
-            printf("%d",d);
-            break;
-        default:
-            printf("Null");
-            break;
-
+        printf("密钥不安全，请重新输入");
+        return 0;
+    }
+    while (i<50)
+    {
+        if (n%i==0)
+        {
+            cnt+=1;
+        }
+        i+=1;
+    }
+    if (cnt<=2)
+    {
+        printf("密钥安全，密码设置成功");
+    }
+    else
+    {
+        printf("密钥不安全，请重新输入");
     }
     return 0;
+
 
 }
