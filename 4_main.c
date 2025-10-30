099264b686dce9af3501bc7b70fc9110527c5429
CrashingCloud <18957760988@163.com>
2025-10-30 21:25:48 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -1,20 +1,29 @@
 //202510304106
 //18957760988@163.com
 //叶子恒
-#include <stdio.h>
-
+#include<stdio.h>
 int main(void)
 {
     int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    if (a+b>c && a+c>b && b+c>a)
+    int i = 100;
+    int sum = 0;
+    int cnt = 0;
+    while (i<=999)
     {
-        printf("可以组成三角形");
-    }
-    else
-    {
-        printf("不能组成三角形");
+        a=i/100;
+        b=i/10%10;
+        c=i%10;
+        sum=a*a*a+b*b*b+c*c*c;
+        if (i==sum && cnt==1)
+        {
+            printf(" %d",sum);
+        }
+        if (i==sum && cnt==0) 
+        {
+            printf("%d",sum);
+            cnt=1;
+        }
+        i+=1;
     }
     return 0;
-
 }
