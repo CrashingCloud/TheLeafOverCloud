9e4052b3ee90384cd0aa5beda2fd8a155c6d78e1
CrashingCloud <18957760988@163.com>
2025-11-05 21:01:05 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -7,21 +7,24 @@ int main(void)
     int row=3,col=3;
     int mat[row][col];
     int a=0;
+    
     for(int i=0;i<row;i++)
     {
         for(int j=0;j<col;j++)
         {
             scanf("%d",&a);
             mat[i][j]=a;
-        }
+        } 
     }
+
     for (int i=0;i<row;i++)
     {
-        for (int j=0; j<col; j++)
+        for (int j=0; j<col-1; j++)
         {
             printf("%d ",mat[i][j]);
         }
-        printf("\r\n");
+        printf("%d\r\n",mat[i][2]);
     }
     return 0;
 }
+
