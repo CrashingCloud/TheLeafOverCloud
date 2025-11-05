11665d2108d0b5cc6999bc6424ac62e686b78d73
CrashingCloud <18957760988@163.com>
2025-11-05 20:24:32 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,41 +1,27 @@
 //202510304106
 //18957760988@163.com
 //叶子恒
-#include<stdio.h> 
+#include<stdio.h>
 int main(void)
 {
-    int n = 0;
-    int k = 0;
-    int m = 50;
-    printf("n=");
-    scanf("%d",&n);
-    if (n>=50 || n<=0)
+    int row=3,col=3;
+    int mat[row][col];
+    int a=0;
+    for(int i=0;i<row;i++)
     {
-        return 0;
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&a);
+            mat[i][j]=a;
+        }
     }
-    if (n!=2 && n % 2 == 0)
+    for (int i=0;i<row;i++)
     {
-        printf("密钥不安全，请重新输入");
-        return 0;
-    }
-    if (n<m)
-    {
-        m=n;
-    }
-    for (int i = 1;i<=m;i+=2)
-    {
-       if (n%i==0)
-       {
-            k+=1;
-       }
-    }
-    if (k==2 || n==2)
-    {
-        printf("密钥安全，密码设置成功");
-    }
-    else
-    {
-        printf("密钥不安全，请重新输入");
+        for (int j=0; j<col; j++)
+        {
+            printf("%d ",mat[i][j]);
+        }
+        printf("\r\n");
     }
     return 0;
-}                                                                                         
+}
