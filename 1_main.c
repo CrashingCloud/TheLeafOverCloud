c596393481e43f7b3664391eef39f4b3a3b8292c
CrashingCloud <18957760988@163.com>
2025-11-19 22:32:04 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,27 +4,11 @@
 #include<stdio.h>
 int main(void)
 {
-    int row=3,col=3;
-    int mat[row][col];
     int a=0;
-    
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<col;j++)
-        {
-            scanf("%d",&a);
-            mat[i][j]=a;
-        } 
-    }
-
-    for (int i=0;i<row;i++)
-    {
-        for (int j=0; j<col-1; j++)
-        {
-            printf("%d ",mat[i][j]);
-        }
-        printf("%d\r\n",mat[i][2]);
-    }
+    scanf("%d",&a);
+    int *p=&a;
+    *p+=10;
+    printf("%d,",a);
+    printf("%d",*p);
     return 0;
 }
-
