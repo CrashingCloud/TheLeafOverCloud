f4c9f909c93cb14c06bec8f886defe702a4a6fd8
CrashingCloud <18957760988@163.com>
2025-11-19 22:33:45 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,35 +2,23 @@
 //18957760988@163.com
 //叶子恒
 #include<stdio.h>
+void swap(int *p,int *q)
+{
+    int temp=0;
+    temp=*p;
+    *p=*q;
+    *q=temp;
+}
 int main(void)
 {
-    int arr[10];
-    int temp;
-    for (int i=0;i<10;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    for (int i=9;i>0;i--)
-    {
-        for (int j=0;j<i;j++)
-        {
-            if (arr[j]>arr[j+1])
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-        }
-    }
-    for (int i=0;i<10;i++)
-    {
-        if (i<9)
-        {
-            printf("%d ",arr[i]);
-        }
-        else
-        {
-            printf("%d",arr[i]);
-        }
-    }
+    int a=0;
+    int b=0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int *p=&a;
+    int *q=&b;
+    swap(p,q);
+    printf("%d ",a);
+    printf("%d",b);
+    return 0;
 }
