307d0529bc013d44d5388e36f50aebdcc68f8423
CrashingCloud <18957760988@163.com>
2025-11-19 22:34:45 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -2,17 +2,33 @@
 //18957760988@163.com
 //叶子恒
 #include<stdio.h>
-int arith(int a1,int an,int step)
-{
-    int n=0;
-    int sum=0;
-    n=(an-a1)/step+1;
-    sum=a1*n+(n-1)*n/2*step;
-    return sum;
-}
+
+void back(int arr[]);
+
 int main(void)
 {
-    int sum=0;
-    sum=arith(1,100,1);
-    printf("%d",sum);
+    int len=5;
+    int arr[len];
+    for (int i=0;i<len;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    back(arr);
+    for (int i=0;i<len-1;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("%d",arr[4]);
+    return 0;
+}
+
+void back(int arr[])
+{
+    int len=5;
+    int *ptr_arr=arr;
+    for (int i=len-1;i>0;i--)
+    {
+        *(ptr_arr+i)=*(ptr_arr+i-1);
+    }
+    *ptr_arr=0;
 }
